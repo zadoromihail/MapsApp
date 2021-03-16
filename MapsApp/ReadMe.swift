61905d95447256240df58caa613723bdcb6036fe
Mihail <zadoromihail@gmail.com>
2021-03-16 21:47:04 +0300

    fix

--- MapsApp/ReadMe.swift
+++ MapsApp/ReadMe.swift
@@ -5,4 +5,17 @@
 //  Created by Михаил Задорожный on 16.03.2021.
 //
 
-import Foundation
+// Для создания .netrc и установки Mapbox
+
+//1    In terminal type: cd ~
+//2    touch .netrc
+//3    open .netrc
+//4    paste below to .netrc with SecretKey machine api.mapbox.com login mapbox password
+//5    Enjoy: pod install will work without erro
+
+
+//Содержимое файла
+
+//machine api.mapbox.com
+//login mapbox
+//password sk.eyJ1IjoibWloYWlsc3ZzOTUiLCJhIjoiY2ttN256MGQ1MTAxdzJucDF0bGpuazExZyJ9.L7OdjhyeDvaENEHVB7gI_w
